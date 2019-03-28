2a1b6a17c8cbdc650ff73e9bdb34760eda4ec276
Gabriel Costa <gabrielncosta208@gmail.com>
2019-03-28 11:45:15 -0300

    Swapping two integers

--- pointers.c
+++ pointers.c
@@ -1,1 +1,25 @@
+// gets two integers inputs from the user then swaps them.
 
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int main(void)
+{   
+    int x, y;
+    printf("x: ");
+    scanf("%i", &x);
+    printf("y: ");
+    scanf("%i", &y);
+    
+    swap(&x, &y);
+    printf("Swapped, now x is %i and y is %i", x, y);
+}
+
+// a function for swapping two integers
+void swap(int *a, int *b) 
+{
+    int swp = *a;
+    *a = *b;
+    *b = swp;
+}   
